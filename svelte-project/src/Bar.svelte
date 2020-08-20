1fe0d0e53ae666ebc11f14f72c15baa11095ded6
SaayaMurakami <s-murakami@unext.jp>
2020-08-20 11:44:19 +0900

    作成したプロジェクトをコミット

---
+++ svelte-project/src/Bar.svelte
@@ -0,0 +1,33 @@
+ <!-- JavaScript -->
+<script>
+    export let label = '';
+    export let value = 0;
+</script>
+
+<!-- HTML -->
+<div class="container">
+    <div class="label">{label}</div>
+    <div class="bar" style="--value: {value}px"></div>
+</div>
+
+<!-- CSS -->
+<style>
+    .container {
+    display: flex;
+    align-items: center;
+    height: 1.5em;
+    padding: 0.2em 0;
+  }
+
+  .label {
+    margin-right: 0.5em;
+    width: 4em;
+    text-align: right;
+  }
+
+  .bar {
+    background-color: white;
+    height: 100%;
+    width: var(--value, 0);
+  }
+</style>
